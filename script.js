e58cbadc8e7137492d97d9eb9d034b72dedf9d6e
D4rkHeart <leonardo.surdez@gmail.com>
2021-09-07 15:37:00 +0200

    [Refactor] with jquery

--- script.js
+++ script.js
@@ -1,51 +1,19 @@
-// import fetch from 'node-fetch';
-//TOKKEN : UnBnWEsYR3AaY3bVZAL1nACrJN8eqZHT1efAY5oa
-var element = document.getElementById("showdatas");
-//element.append("Some text");
-
-
 function showdata(data){
     console.log(data[0])
-    for (var i = 0; i < data.length; i++){
+    for (var i = 0; i < data.length; i++) {
         console.log(data[i].date)
         console.log(data[i].title)
-
-        let title = document.createElement("p")
-        title.append("Title: "+data[i].title)
-        element.append(title)
-
-        let date = document.createElement("p")
-        date.append("date: "+data[i].date)
-        element.append(date)
-
-        let picoftheday = document.createElement("img")
-        picoftheday.width=240
-        picoftheday.toto = "tata"
-        picoftheday.src = data[i].url
-        element.append(picoftheday)
+        $("#showdatas").append(`<apod>Title: ${data[i].title}<br>Date: ${data[i].date}<br><img src="${data[i].url}" width="240px" /></apod><hr>`)
     }
-    
 }
 
 async function fetchinformation() {
-    let apiresponse = await fetch("https://api.nasa.gov/planetary/apod?api_key=UnBnWEsYR3AaY3bVZAL1nACrJN8eqZHT1efAY5oa&start_date=2021-09-03")
-    if(apiresponse.ok){
+    let apiresponse = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2021-09-03")
+    if (apiresponse.ok) {
         let data = await apiresponse.json()
         showdata(data);
-    
-    }
-    else{
+    } else {
         console.log("API ERROR : " + apiresponse.statusText)
     }
 }
 fetchinformation()
-
-//displays information
-
-
-
-
-
-
-//contain editor permet d'append, ou autre le contenu sous forme d'html
-//1. afficher les datas sur l'html avec "insert child etc... modifier le dom" 2.mise en page 
